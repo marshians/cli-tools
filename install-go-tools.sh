a9af39769ccee666f67a281938798cac363fdb40
Joshua Marsh (icub3d) <joshua@themarshians.com>
2020-04-23 12:42:47 -0600

    remove golangci-lint

--- install-go-tools.sh
+++ install-go-tools.sh
@@ -7,7 +7,7 @@ go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/godoc 
 go get -u github.com/mdempsky/gocode 
 go get -u github.com/rogpeppe/godef 
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint 
+#go get -u github.com/golangci/golangci-lint/cmd/golangci-lint 
 go get -u github.com/cweill/gotests/... 
 go get -u golang.org/x/tools/gopls 
 go get -u github.com/mikefarah/yq
